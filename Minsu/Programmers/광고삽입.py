d2dc7fb51a77b9d023e658946afb2db53cde5b5f
tjatn304905 <tjatn304905@gmail.com>
2021-05-08 18:02:11 +0900

    준규님 해설 참고해서 풀이

--- Minsu/Programmers/광고삽입.py
+++ Minsu/Programmers/광고삽입.py
@@ -1,12 +1,7 @@
-play_time = "02:03:55"
-adv_time = "00:14:15"
-logs = ["01:20:15-01:45:14", "00:40:31-01:00:00", "00:25:50-00:48:29", "01:30:59-01:53:29", "01:37:44-02:02:30"]
-
-
 # 시간을 초로 환산하는 함수
 def tosecond(time):
-    h, m, s = map(int, time.split(':'))
-    return (h * 3600 + m * 60 + s)
+    seconds = int(time[0:2]) * 3600 + int(time[3:5]) * 60 + int(time[6:8])
+    return seconds
 
 
 # 초에서 시간단위로 환산하는 함수
@@ -31,35 +26,42 @@ def tohour(time):
 
 
 def solution(play_time, adv_time, logs):
+
     P = tosecond(play_time)  # 전체 재생시간
     A = tosecond(adv_time)  # 광고 시간
 
-    total = [0] * P # 전체 플레이 시간을 초로 나눈 리스트
+    total = [0] * (P+1)  # 전체 플레이 시간을 초로 나눈 리스트
 
-    # 새로운 재생기록 리스트 만들기
-    new_logs = []
+    # 로그기록에서 시작시간에 1, 끝나는 시간에 -1 넣어주기
     for log in logs:
-        new_logs += (log.split('-'))  # '-'기준으로 쪼개서 넣어주기
+        start, end = map(str, log.split('-'))  # '-'기준으로 쪼개서 넣어주기
+        start = tosecond(start)
+        end = tosecond(end)
 
-    # 새로운 재생기록들을 초 단위로 바꿔주기
-    for i in range(len(new_logs)):
-        new_logs[i] = tosecond(new_logs[i])
+        total[start] += 1
+        total[end] -= 1
 
-    # 재생된 시간에 1씩 추가해주기
-    for i in range(len(new_logs)//2):
-        for j in range(new_logs[i*2], new_logs[i*2+1]):
-            total[j] += 1
+    # 당시 시청하고 있는 시청자 수 구하기
+    for i in range(1, P+1):
+        total[i] = total[i-1] + total[i]
 
-    results = []  # 결과들을 담는 리스트
-    for start in range(P):
-        end = (start + A if start + A <= P else P)  # 광고 끝 시간
-        if sum(total[start:end]) not in results:
-            results.append((sum(total[start:end])))
-            if max(results) == sum(total[start:end]):
-                max_idx = start
+    # 초당 방송 누적 시청 수 구하기
+    for i in range(1, P+1):
+        total[i] = total[i-1] + total[i]
 
-    answer = tohour(max_idx)
+    # 최대 시간, 시작시간 초기화하기
+    # 처음에 이부분을 간과했다가 엣지케이스가 생겼습니다.
+    max_time = total[A-1]
+    max_start = 0
+    
+    # 순회를 돌면서 최대시간 및 그때의 시작시간 구하기
+    for i in range(P+1-A):
+        if max_time < total[i+A] - total[i]:
+            max_time = total[i+A] - total[i]
+            max_start = i+1
+
+    answer = tohour(max_start)
     return answer
 
 
-print(solution(play_time, adv_time, logs))
+print(solution("02:03:55", "00:14:15", ["01:20:15-01:45:14", "00:40:31-01:00:00", "00:25:50-00:48:29", "01:30:59-01:53:29", "01:37:44-02:02:30"]))
